197835b1a55d42c1fd86f0c4a50a949048788ed3
Pascal Münst <pam@metanet.ch>
2014-05-13 11:16:08 +0200

    Added resetChecked

---
+++ lib/ch/metanet/formHandler/renderer/CallbackInputFieldRenderer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ch\metanet\formHandler\renderer;
+
+use ch\metanet\formHandler\field\InputField;
+
+/**
+ * @author Pascal Muenst <entwicklung@metanet.ch>
+ * @copyright Copyright (c) 2014, METANET AG
+ * @version 1.0.0
+ */
+class CallbackInputFieldRenderer extends InputFieldRenderer {
+	protected $callback;
+
+	/**
+	 * @param array $callback A valid callback to render the InputField
+	 */
+	public function __construct(array $callback) {
+		$this->callback = $callback;
+	}
+
+	/**
+	 * @param InputField $field The InputField to render
+	 * @return string The rendered HTML for the InputField
+	 */
+	public function render(InputField $field) {
+		return call_user_func($this->callback, $field);
+	}
+}
+
+/* EOF */
