13b033adb24e4f8e20d82a97062dc79364c99d4b
TiMESPLiNTER <github@timesplinter.ch>
2014-06-25 13:32:15 +0200

    Added new option to check MX records

--- lib/ch/metanet/formHandler/rule/ValidEmailAddressRule.php
+++ lib/ch/metanet/formHandler/rule/ValidEmailAddressRule.php
@@ -11,6 +11,14 @@ use ch\metanet\formHandler\field\FormField;
  * @version 1.0.0
  */
 class ValidEmailAddressRule  extends Rule {
+	protected $checkMx;
+
+	function __construct($errorMessage, $checkMx = true) {
+		parent::__construct($errorMessage);
+
+		$this->checkMx = $checkMx;
+	}
+
 	/**
 	 * @param FormField $field
 	 * @return bool
@@ -19,7 +27,19 @@ class ValidEmailAddressRule  extends Rule {
 		if($field->isValueEmpty() === true)
 			return true;
 
-		return (filter_var($field->getValue(), FILTER_VALIDATE_EMAIL) !== false);
+		$fieldValue = $field->getValue();
+		$emailValid = filter_var($fieldValue, FILTER_VALIDATE_EMAIL);
+
+		if($emailValid === false)
+			return false;
+
+		if($this->checkMx === false)
+			return true;
+
+		$domain = substr($fieldValue, strrpos($fieldValue, '@') + 1);
+		$mxRecords = array();
+
+		return getmxrr(idn_to_ascii($domain), $mxRecords);
 	}
 }
 
