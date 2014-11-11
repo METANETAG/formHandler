668422d5a13b125ab264640419c53d29a13ef170
TiMESPLiNTER <github@timesplinter.ch>
2014-11-11 10:53:05 +0100

    Updated renderer, updated to PHPUnit 4.2.x

--- lib/ch/metanet/formHandler/renderer/RadioOptionsFieldRenderer.php
+++ lib/ch/metanet/formHandler/renderer/RadioOptionsFieldRenderer.php
@@ -9,20 +9,39 @@ use ch\metanet\formHandler\field\OptionsField;
  * @copyright Copyright (c) 2014, METANET AG
  * @version 1.0.0
  */
-class RadioOptionsFieldRenderer extends OptionsFieldRenderer {
+class RadioOptionsFieldRenderer extends OptionsFieldRenderer
+{
 	/**
 	 * @param OptionsField $field
 	 * @return string
 	 * @throws \UnexpectedValueException
 	 */
-	public function render(OptionsField $field) {
+	public function render(OptionsField $field)
+	{
 		$required = ($field->hasRule('ch\metanet\formHandler\rule\RequiredRule') === true)?' aria-required="true"':null;
 		$html = '<ul' . $this->getAttributesAsHtml() . '>';
 
 		foreach($field->getOptions() as $key => $val) {
+			$label = $val;
+			$htmlAttributes = '';
+			$htmlBefore = null;
+			$htmlAfter = null;
+
+			if(is_array($val) === true) {
+				$label = $val['label'];
+				$htmlAfter = isset($val['html_after'])?$val['html_after']:null;
+				$htmlBefore = isset($val['html_before'])?$val['html_before']:null;
+
+				if(isset($val['attributes']) === true) {
+					foreach($val['attributes'] as $attrName => $attrValue) {
+						$htmlAttributes .= ' ' . $attrName . '="' . $attrValue . '"';
+					}
+				}
+			}
+
 			$attrId = ' id="' . $field->getName() . '-' . $key . '"';
 			$checked = ($key == $field->getValue())?' checked':null;
-			$html .= '<li><label><input type="radio" name="' . $field->getName() . '" value="' . $key . '"' . $attrId . $checked . $required . '> ' . $val . '</label></li>';
+			$html .= '<li>' . $htmlBefore . '<label><input type="radio" name="' . $field->getName() . '" value="' . $key . '"' . $attrId . $checked . $htmlAttributes . $required . '> ' . $label . '</label>' . $htmlAfter . '</li>';
 		}
 
 		$html .= '</ul>';
